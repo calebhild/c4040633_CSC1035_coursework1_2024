bc77416c91d0ac4e5186b63b44743d2023a8b3af
Caleb Hildrew <c4040633@newcastle.ac.uk>
2025-02-16 20:20:20 +0000

    Finishing Reporting.java, Writing Methods: Manufacturer with Highest Rev, Car Models above a Certain Price

--- Reporting.java
+++ Reporting.java
@@ -33,4 +33,36 @@ public class Reporting {
         }
         return mostExpensive;
     }
+
+public Manufacturer getManufacturerWithHighestRevenue(String type) {                                         // Find the manufacturer with the most revenue for a given car type
+    if (manufacturers.size() == 0) {                                                                         // If no manufacturers exist, return null
+        return null;
+    }
+
+    Manufacturer highestManufacturer = null;
+    double highestRevenue = 0;
+
+    for (int i = 0; i < manufacturers.size(); i++) {
+        double revenue = manufacturers.get(i).getTotalRevenueByType(type);
+
+        if (revenue > highestRevenue) {                                                                      // Check if revenue is higher
+            highestRevenue = revenue;
+            highestManufacturer = manufacturers.get(i);
+        }
+    }
+    return highestManufacturer;
+}
+
+public ArrayList<CarModel> getCarModelsAbovePrice(double price) {                                            // Get all car models that are above a certain price
+    ArrayList<CarModel> expensiveCars = new ArrayList<>();
+
+    for (int i = 0; i < manufacturers.size(); i++) {
+        ArrayList<CarModel> carsFromManufacturer = manufacturers.get(i).getCarModelsAbovePrice(price);
+
+        for (int j = 0; j < carsFromManufacturer.size(); j++) {                                             // Add all expensive cars from this manufacturer to the list
+            expensiveCars.add(carsFromManufacturer.get(j));
+        }
+    }
+    return expensiveCars;
+}
 }
